d128bcaa592ab9f0192f266549834914390bed98
umesh choudhary <80300968+itumesh@users.noreply.github.com>
2021-06-24 18:36:15 +0530

    Update custom-map.js

--- js/custom-map.js
+++ js/custom-map.js
@@ -177,7 +177,6 @@ jQuery(document).ready(function ($) {
 
   //set google map options
 
-  // ------------Hiding Map Umesh-------------
   //   var map_options = {
   //     center: new google.maps.LatLng(latitude, longitude),
   //     zoom: map_zoom,
@@ -202,26 +201,26 @@ jQuery(document).ready(function ($) {
   //     icon: marker_url,
   //   });
 
-  //   //add custom buttons for the zoom-in/zoom-out on the map
-  //   function CustomZoomControl(controlDiv, map) {
-  //     //grap the zoom elements from the DOM and insert them in the map
-  //     var controlUIzoomIn = document.getElementById('cd-zoom-in'),
-  //       controlUIzoomOut = document.getElementById('cd-zoom-out');
-  //     controlDiv.appendChild(controlUIzoomIn);
-  //     controlDiv.appendChild(controlUIzoomOut);
+  //add custom buttons for the zoom-in/zoom-out on the map
+  function CustomZoomControl(controlDiv, map) {
+    //grap the zoom elements from the DOM and insert them in the map
+    var controlUIzoomIn = document.getElementById('cd-zoom-in'),
+      controlUIzoomOut = document.getElementById('cd-zoom-out');
+    controlDiv.appendChild(controlUIzoomIn);
+    controlDiv.appendChild(controlUIzoomOut);
 
-  //     // Setup the click event listeners and zoom-in or out according to the clicked element
-  //     google.maps.event.addDomListener(controlUIzoomIn, 'click', function () {
-  //       map.setZoom(map.getZoom() + 1);
-  //     });
-  //     google.maps.event.addDomListener(controlUIzoomOut, 'click', function () {
-  //       map.setZoom(map.getZoom() - 1);
-  //     });
-  //   }
+    // Setup the click event listeners and zoom-in or out according to the clicked element
+    // google.maps.event.addDomListener(controlUIzoomIn, 'click', function () {
+    //   map.setZoom(map.getZoom() + 1);
+    // });
+    // google.maps.event.addDomListener(controlUIzoomOut, 'click', function () {
+    //   map.setZoom(map.getZoom() - 1);
+    // });
+  }
 
-  //   var zoomControlDiv = document.createElement('div');
-  //   var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+  var zoomControlDiv = document.createElement('div');
+  var zoomControl = new CustomZoomControl(zoomControlDiv, map);
 
-  //   //insert the zoom div on the top left of the map
-  //   map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+  //insert the zoom div on the top left of the map
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 });
